b1b4b4294ad2bfb26b0e2fca5d731b67eb1ab737
Bui Sy Nguyen <nguyenbs@projectkit.net>
2015-10-02 13:49:02 +0700

    Add tests for travis-ci

---
+++ tests/JsonHelperTest.php
@@ -0,0 +1,42 @@
+<?php
+use fproject\amqp\ActivityNoticeSerializer;
+use fproject\common\utils\JsonHelper;
+include_once('TestModel01.php');
+include_once('TestModel02.php');
+class JsonHelperTest extends PHPUnit_Framework_TestCase
+{
+    private $params = [];
+
+    public function testJsonEncode()
+    {
+        $this->params = [
+            'activityNotice' => [
+                'TestModel02' => [
+                    'notifyActions' => '*',
+                    'serializeAttributes' => 'jsonData,model1.field1,model1.field3,workCalendar,resources,projectTasks'
+                ],
+            ]
+        ];
+
+        $model = new TestModel02();
+        $model->model1 = new TestModel01();
+        $model->model1->field1 = "ABC";
+        $model->model1->field2 = "XYZ";
+        $model->model1->field3 = "GHI";
+        $model->projectTasks = ['abc','def'];
+
+        $json = JsonHelper::encode($model);
+        $obj = json_decode($json);
+        $this->assertNull($obj->jsonData);
+        $this->assertNull($obj->workCalendar);
+        $this->assertNull($obj->resources);
+        $this->assertTrue(is_array($obj->projectTasks));
+        $this->assertEquals("abc",$obj->projectTasks[0]);
+        $this->assertEquals("def",$obj->projectTasks[1]);
+        $this->assertNotNull($obj->model1);
+        $this->assertEquals("ABC",$obj->model1->field1);
+        $this->assertObjectNotHasAttribute("field2",$obj->model1);
+        $this->assertEquals("GHI",$obj->model1->field3);
+    }
+}
+?>
---
+++ tests/TestModel01.php
@@ -0,0 +1,7 @@
+<?php
+class TestModel01 {
+    public $field1;
+    public $field2;
+    public $field3;
+    public $_explicitType;
+}
