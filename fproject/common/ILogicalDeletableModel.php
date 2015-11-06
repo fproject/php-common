a0b6c97b33803c41e61a88a1bcba70c41faf1df1
Bui Sy Nguyen <nguyenbs@projectkit.net>
2015-11-06 11:52:52 +0700

    Change namspace for ILogicalDeletableModel to namespace fproject\common

--- fproject/common/ILogicalDeletableModel.php
+++ fproject/common/ILogicalDeletableModel.php
@@ -16,6 +16,9 @@
 // limitations under the License.
 //
 ///////////////////////////////////////////////////////////////////////////////
+
+namespace fproject\common;
+
 interface ILogicalDeletableModel
 {
     /**
