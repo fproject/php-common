ba5cf503f0ad9282df6e851550eff4e8980a645f
Bui Sy Nguyen <nguyenbs@projectkit.net>
2016-01-19 10:14:02 +0700

    Update PHPDoc

--- fproject/common/ILogicalDeletableModel.php
+++ fproject/common/ILogicalDeletableModel.php
@@ -1,7 +1,7 @@
 <?php
 ///////////////////////////////////////////////////////////////////////////////
 //
-// © Copyright f-project.net 2010-present.
+// Â© Copyright f-project.net 2010-present.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 // limitations under the License.
 //
 ///////////////////////////////////////////////////////////////////////////////
+namespace fproject\common\utils;
 
 namespace fproject\common;
 
